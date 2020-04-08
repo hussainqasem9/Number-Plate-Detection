cdddbb462721b96ef35b3f4f08bd5c74542d82b8
hussainqasem9 <hussainqasem68@gmail.com>
2020-04-08 07:14:24 +0300

    massage rpat

--- Plate_detection.m
+++ Plate_detection.m
@@ -7,7 +7,7 @@ imgray = rgb2gray(im);
 imbin = imbinarize(imgray);
 im = edge(imgray, 'prewitt');
 
- %Below steps are to find location of number plate
+ %Below steps are to find location of number platemojpopojpo
 Iprops=regionprops(im,'BoundingBox','Area', 'Image');
 area = Iprops.Area;
 count = numel(Iprops);
